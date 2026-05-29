b17e18aa0550da8c6fba4e3728a1fe495b390e7f
Muhammad Hashim <hashimgujjar4447@gmail.com>
2026-05-29 11:06:29 +0500

    Changing url of backend

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://business-nexus-backend-ddf511e82d76.herokuapp.com/api/v1",
 
   withCredentials: true,
 });
@@ -40,7 +40,7 @@ axiosInstance.interceptors.response.use(
 
       try {
         const response = await axios.post(
-          "http://localhost:8080/api/v1/auth/refresh",
+          "https://business-nexus-backend-ddf511e82d76.herokuapp.com/api/v1/auth/refresh",
           {},
           {
             withCredentials: true,
